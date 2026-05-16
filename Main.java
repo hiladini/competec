7a60a00997c53e91ec171b6bec27a8ac8b5028e4
hiladini <hhmonfardini@gmail.com>
2026-05-16 10:22:17 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			    int codigo = sc.nextInt();
+			    
+			    if  (codigo == 1 && ){
+	  System.out.println("dormi bem,vou gabaritar");
+	    }
+	       else {
+	  System.out.println("nao dormi bem,vou ter que estudar mais");
+	}
+	}
+}
