b24ed58a51a5fd0b85c5fd6d4d699bfe3231627c
hiladini <hhmonfardini@gmail.com>
2026-05-16 11:35:14 -0300

    Add files via upload

---
+++ Main(1).java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+			    System.out.println("Digite uma palavra: ");
+			    String palavra = scanner.nextLine();
+			    
+			    int contador = 0;
+			    
+			    while(contador <=4 ){
+			        
+			        System.out.println(contador + "." + palavra);
+			        contador++;
+
+			    }
+			    
+	}
+}
