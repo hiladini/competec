fc4a1a716f32008ceef571ae097884deeaa4f5fd
hiladini <hhmonfardini@gmail.com>
2026-06-27 10:55:42 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,27 +2,8 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-		int numero;
-		boolean maisQueTres = false;
-		int frequencia [] = new int [10];
-		
-		Scanner sc = new Scanner(System.in);
-		
-		for(int i = 0; i < 5; i++){
-		    numero = sc.nextInt();
-		    frequencia[numero]++;
-		}
-		for(int i = 0; i<10; i++){
-		    if(frequencia[i] >= 3){
-		        maisQueTres = true;
-		    }
-		}
-		if(maisQueTres == true){
-		    System.out.println("Sim");
-		}
-		else{
-		    System.out.println("Não");
-		}
-		
+    Scanner scan = new Scanner(System.in);
+    String entrada = scan.nextLine( );
+    System.out.println(entrada); 
 	}
 }
